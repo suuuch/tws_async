9060025527226fc1262140e63999624e295bf9a2
Longfei.shen <shenlongfei@gmail.com>
2018-07-09 16:26:17 +0800

    Futures Open Interesting

--- samples/tickstreamer_demo.py
+++ samples/tickstreamer_demo.py
@@ -1,50 +1,74 @@
+from pprint import pprint
+
 import ibapi
-from tws_async import TWSClient, iswrapper, Stock, Forex, CFD
+from ibapi.contract import Contract
+from ibapi.common import TickerId
+from ibapi.ticktype import TickType
+
+from tws_async import TWSClient, iswrapper, Future, AVAILABLE_TICK_TYPES, ROOT_PATH
 
 
 class TickStreamer(TWSClient):
     """
     Get live streaming tick data from TWS or gateway server.
     """
+
     def __init__(self):
         TWSClient.__init__(self)
         self.accountName = ''
         self._reqIdSeq = 0
         self._reqId2Contract = {}
+        self.tick_data = {}
 
     def subscribe(self):
         contracts = [
-            Stock('GOOG'),
-            Stock('INTC', primaryExchange='NASDAQ'),
-            Forex('EURUSD'),
-            CFD('IBUS500')
+            Future(localSymbol='CLQ8', exchange='NYMEX', currency='USD')
         ]
         for contract in contracts:
             reqId = self.getReqId()
             self._reqId2Contract[reqId] = contract
-            self.reqMktData(reqId, contract, genericTickList='', snapshot=False,
-                    regulatorySnapshot=False, mktDataOptions=[])
+            self.reqMktData(reqId, contract, genericTickList='588', snapshot=False,
+                            regulatorySnapshot=False, mktDataOptions=[])
 
     @iswrapper
     def connectAck(self):
-        self.reqAccountUpdates(1, '')
-        self.reqOpenOrders()
-        self.reqPositions()
+        # self.reqAccountUpdates(1, '')
+        # self.reqOpenOrders()
+        # self.reqPositions()
+        pass
 
     @iswrapper
     def tickPrice(self, reqId: int,
-            tickType: ibapi.ticktype.TickType,
-            price: float,
-            attrib: ibapi.common.TickAttrib):
+                  tick_type: TickType,
+                  price: float,
+                  attrib: ibapi.common.TickAttrib):
         contract = self._reqId2Contract[reqId]
-        print('{} price {}'.format(contract.symbol, price))
+        type_name = AVAILABLE_TICK_TYPES.get(tick_type, TickType)
+        # print('{} {} price {}'.format(contract.symbol,type_name, price))
+        self.tick_data.update({type_name: price})
 
     @iswrapper
     def tickSize(self, reqId: int,
-            tickType: ibapi.ticktype.TickType,
-            size: int):
+                 tick_type: TickType,
+                 size: int):
         contract = self._reqId2Contract[reqId]
-        print('{} size {}'.format(contract.symbol, size))
+        type_name = AVAILABLE_TICK_TYPES.get(tick_type, TickType)
+        # print('{} {} size {}'.format(contract.symbol, type_name,size))
+        self.tick_data.update({type_name: size})
+
+    @iswrapper
+    def tickGeneric(self, reqId: TickerId, tick_type: TickType, value: float):
+        type_name = AVAILABLE_TICK_TYPES.get(tick_type, TickType)
+        # print(tick_type,type_name,value)
+        self.tick_data.update({type_name: value})
+        pass
+
+    def tickString(self, reqId: TickerId, tick_type: TickType, value: str):
+        type_name = AVAILABLE_TICK_TYPES.get(tick_type, TickType)
+        # print(tick_type,type_name,value)
+        self.tick_data.update({type_name: value})
+        if tick_type == 45:
+            pprint(self.tick_data)
 
     @iswrapper
     def updateAccountTime(self, timeStamp: str):
@@ -56,14 +80,14 @@ class TickStreamer(TWSClient):
 
     @iswrapper
     def updateAccountValue(self, key: str, val: str, currency: str,
-            accountName: str):
+                           accountName: str):
         print('Account update: {} = {} {}'.format(key, val, currency))
 
     @iswrapper
     def position(self, account: str,
-            contract: ibapi.contract.Contract,
-            position: float,
-            avgCost: float):
+                 contract: Contract,
+                 position: float,
+                 avgCost: float):
         print('Position: {} {} @ {}'.format(position, contract.symbol, avgCost))
 
     @iswrapper
@@ -72,6 +96,6 @@ class TickStreamer(TWSClient):
 
 
 tws = TickStreamer()
-tws.connect(host='127.0.0.1', port=7497, clientId=1)
+tws.connect(host='127.0.0.1', port=4001, clientId=1)
 tws.subscribe()
 tws.run()
